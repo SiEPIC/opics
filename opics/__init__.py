7a7f45a6348adb17f554aec2948702f49f7dd34e
Mustafa Hammood <mustafa@ece.ubc.ca>
2020-08-06 17:25:53 -0700

    update requirements
    scipy and numpy

--- opics/__init__.py
+++ opics/__init__.py
@@ -1,5 +1,12 @@
 """Top-level package for OPICS."""
 
-__author__ = """Mustafa Hammood"""
-__email__ = 'mustafa@siepic.com'
+__author__ = """Jaspreet Jhoja"""
+__email__ = 'Jaspreet@siepic.com'
 __version__ = '0.1.5'
+
+#initialize OPICS package
+
+name = "opics"
+from opics.version_ import __version__
+__all__ = ['library', 'network', 'data']
+from . import *
