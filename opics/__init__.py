5fcddcc56852824fa2d74338c2d0f8703caa0422
Jaspreet Jhoja <jaspreetj@ece.ubc.ca>
2020-10-09 22:00:02 -0700

    Fix - netlist reader
    - renamed opics.libs -> opics.libraries
    - fixed references for the netflist reader example

--- opics/__init__.py
+++ opics/__init__.py
@@ -1,12 +1,12 @@
 """Top-level package for OPICS."""
 
-from opics import libs
+from opics import libraries
 from opics.network import Network
 from opics.utils import universal_sparam_filereader, LUT_processor, LUT_reader, netlistParser
 from opics.globals import c, f
 
-"Register ebeam library"
-import opics.libs.ebeam
+"Register libraries"
+import opics.libraries.ebeam
 
 __author__ = 'Jaspreet Jhoja'
 __email__ = 'Jaspreet@siepic.com'
@@ -16,7 +16,7 @@ __version__ = '0.1.6'
 
 name = "opics"
 
-__all__ = ['Network', 'libs', "globals"]
+__all__ = ['Network', 'libraries', "globals"]
 
 
 print("OPICS version", __version__)
