a8b148d6633ffe5e472c7919bf74761990f5c67e
Jaspreet Jhoja <jaspreetj@ece.ubc.ca>
2020-11-24 19:39:13 -0800

    added tests and pytest coverage

--- tests/test_components.py
+++ tests/test_components.py
@@ -1,10 +1,10 @@
 import numpy as np
 import pytest
 import opics as op
-from opics.libraries.ebeam import components, component_factory
+from opics.libraries.ebeam import components_list, component_factory
 
 
-@pytest.mark.parametrize("component_type", components)
+@pytest.mark.parametrize("component_type", components_list)
 def test_properties_components(component_type, num_regression):
     w = np.linspace(1.52, 1.58, 3) * 1e-6
     f = op.c / w
---
+++ tests/test_globals.py
@@ -0,0 +1,5 @@
+import opics as op
+import pytest 
+
+def test_c():
+    assert op.globals.c == 299792458
